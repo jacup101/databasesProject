ef85422764fc4a86d887cf8bdc6c4e6c5926960b
aamanhey <amanhey@oxy.edu>
2022-04-22 13:09:11 -0700

    Update HeaderComponent.js
    Removed useless constructor

--- src/frontend/src/components/HeaderComponent.js
+++ src/frontend/src/components/HeaderComponent.js
@@ -1,11 +1,6 @@
 import React from 'react';
 
 class HeaderComponent extends React.Component {
-
-    constructor(props){
-        super(props)
-    };
-
     render(){
       return(
         <div>
